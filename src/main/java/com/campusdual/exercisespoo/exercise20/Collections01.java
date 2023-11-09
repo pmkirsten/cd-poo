0ddf5ca3bff70bb6640c582c07ff1192c1163c0f
albertoperezitx <albertopc@ext.zara.com>
2023-11-09 11:12:24 +0100

    Refactor for pushing test

--- src/main/java/com/campusdual/exercisespoo/exercise20/Collections01.java
+++ src/main/java/com/campusdual/exercisespoo/exercise20/Collections01.java
@@ -10,24 +10,20 @@ import com.campusdual.exercisespoo.exercise14.Teacher;
 
 public class Collections01 {
 
-    public static void main(String[] args) {
-        List<Person> stringList = new ArrayList<>();
+  public static void main(String[] args) {
+    List<Person> stringList = new ArrayList<>();
 
-        stringList.add(new Person("John", "Smith"));
-        stringList.add(new Teacher("María", "Montessori", "Educación"));
-        stringList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
-        stringList.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+    stringList.add(new Person("John", "Smith"));
+    stringList.add(new Teacher("María", "Montessori", "Educación"));
+    stringList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+    stringList.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
 
-//        for (Person p : stringList) {
-//            p.getDetails();
-//        }
+    //        for (int i = 0; i < stringList.size(); i++){
+    //            stringList.get(i).getDetails();
+    //        }
 
-//        for (int i = 0; i < stringList.size(); i++){
-//            stringList.get(i).getDetails();
-//        }
-
-        for( Person p : stringList){
-            p.getDetails();
-        }
+    for (Person p : stringList) {
+      p.getDetails();
     }
+  }
 }
